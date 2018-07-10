d659e39ade1f8e99188f748c7d402ba057878b44
Zahid Farooq <zfarooq@tams.com>
2018-07-10 15:37:36 +0500

    Set up start script

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
 app.use((req, res, next)=>{
     var now = new Date().toString();
     console.log(`${now}`);
     next();
 });
-// app.use((req, res, next)=>{
-//     res.render('maintance.hbs');
-// });
+
 hbs.registerHelper('getCurrentYear',()=>{
     return new Date().getFullYear();
 })
+
 app.get('/',(req, res)=>{
     res.render('home.hbs', {
         pageTitle:'Home Page',
@@ -27,4 +29,6 @@ app.get('/about',(req, resp)=>{
         pageTitle:'About Page',
     });   
 });
-app.listen(3000);
+app.listen(port, ()=> {
+    console.log(`Server is running at ${port}`);
+});
